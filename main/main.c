8e7dfe8cb2dc8441107b6328c6463118f3aa327f
michael <michael.alexander@kubide.es>
2024-06-16 13:30:55 +0200

    fix:library

--- main/main.c
+++ main/main.c
@@ -80,7 +80,7 @@ void app_main(void)
         ESP_ERROR_CHECK(nvs_flash_erase());
         err = nvs_flash_init();
     }
-    esp_pm_config_esp32_t config_power_mode = {
+    esp_pm_config_t config_power_mode = {
         .max_freq_mhz = 240,
         .min_freq_mhz = 80,
         .light_sleep_enable = true,
