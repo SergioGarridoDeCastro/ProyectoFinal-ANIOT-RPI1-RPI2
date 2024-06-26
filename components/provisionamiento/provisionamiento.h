4c81da59e2d3e6b61c27efcb154d6bfc588ac92d
SergioGarridoDeCastro <sgarridodecastro@gmail.com>
2024-06-26 18:46:31 +0200

    Provisionamiento con QR y WiFi modo STA

---
+++ components/provisionamiento/provisionamiento.h
@@ -0,0 +1,28 @@
+#ifndef PROVISIONAMIENTO
+#define PROVISIONAMIENTO
+
+#include "esp_event.h"
+
+typedef struct {
+    char* wifi_ssid;
+    char* wifi_pass;
+    char* data_piso;
+    char* data_aula;
+    char* data_url;
+} prov_info_t;
+
+enum {
+    PROVISION_DONE,
+    PROVISION_ERROR
+};
+
+ESP_EVENT_DECLARE_BASE(PROVISION_EVENT);
+
+esp_err_t init_provision(void *event_handler);
+esp_err_t custom_prov_data_handler(uint32_t session_id, const uint8_t *inbuf, ssize_t inlen, uint8_t **outbuf, ssize_t *outlen, void *priv_data);
+prov_info_t* get_wifi_info();
+void start_provisioning();
+void stop_provisioning();
+
+
+#endif
---
+++ components/wifi_station/wifi_station.h
@@ -0,0 +1,17 @@
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+
+esp_err_t wifi_init_sta(void *wifi_handler);           
+esp_err_t wifi_disconnect();                     
+esp_err_t wifi_connect(char *ssid, char *password);
